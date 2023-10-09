025c07568b726f98abde09562263668db7606338
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-10-09 17:32:59 +0200

    passwordValidation

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -9,7 +9,7 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
 export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
-
+  if (password.length !== 0) return false;
   if (password.match(/[a - z][A - Z]/)) return true;
   if (password.match(/\w/)) return false;
   if (password.match(/\d+/)) return false;
